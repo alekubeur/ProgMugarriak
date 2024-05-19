16e5776b5fdfefdeecaa7d1dba2a63a1cdbf2c69
alekubeur <alekubeur23wg@ikzubirimanteo.com>
2024-05-19 14:29:00 +0200

    Argazkilari

--- Mugarria4_Aritz/src/ArgazkilariBiztaratzaile.java
+++ Mugarria4_Aritz/src/ArgazkilariBiztaratzaile.java
@@ -2,9 +2,33 @@ import org.jdesktop.swingx.JXDatePicker;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+
 
 public class ArgazkilariBiztaratzaile extends JFrame {
+    String url = "jdbc:mysql://localhost:3306/mugarri4";
+    String usuario = "root";
+    String contrasena = "aritz123";
+    Connection conn;
+    Date data;
+    public void konexioa(){
+
+
+
+        try{
+                conn= DriverManager.getConnection(url, usuario, contrasena);
+
+            System.out.println("Konektatuta");
+        } catch (SQLException e) {
+            System.out.println("Errorea");
+            e.printStackTrace();
+        }
+    }
 public void interfazea() {
+    konexioa();
 
     setLayout(new GridLayout(2, 2));
 
@@ -18,7 +42,12 @@ public void interfazea() {
     goiEskPanel.add(new JLabel("Argazkiak honen ondoren:"));
     JXDatePicker datePicker = new JXDatePicker();
     goiEskPanel.add(datePicker);
+    datePicker.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            data = (Date) datePicker.getDate();
 
+        }
+    });
     JList list = new JList();
     JScrollPane scrollPane = new JScrollPane(list);
 
@@ -33,6 +62,24 @@ public void interfazea() {
     setSize(400, 400);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setVisible(true);
+
+
+    try {
+        konexioa();
+        String consulta = "SELECT Izena FROM Argazkilari";
+        PreparedStatement ps = conn.prepareStatement(consulta);
+        ResultSet rs = ps.executeQuery();
+
+
+        while (rs.next()) {
+            comboBox.addItem(rs.getString("Izena"));
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+
+
 }
+
 }
 
--- Mugarria4_Aritz/src/Main.java
+++ Mugarria4_Aritz/src/Main.java
@@ -4,17 +4,7 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/mugarri4";
-        String usuario = "root";
-        String contrasena = "aritz123";
 
-        try (
-                Connection conexion = DriverManager.getConnection(url, usuario, contrasena)) {
-            System.out.println("Konektatuta");
-        } catch (SQLException e) {
-            System.out.println("Errorea");
-            e.printStackTrace();
-        }
 
 ArgazkilariBiztaratzaile a1=new ArgazkilariBiztaratzaile();
         a1.interfazea();
